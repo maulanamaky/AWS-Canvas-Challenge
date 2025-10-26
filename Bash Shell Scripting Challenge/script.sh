86e40d472d3a483c1ec1739eecaae05ff5cc8ad8
maulanamaky <159880655+maulanamaky@users.noreply.github.com>
2025-10-26 10:56:37 +0700

    Update: Add Documentation Code

--- Bash Shell Scripting Challenge/script.sh
+++ Bash Shell Scripting Challenge/script.sh
@@ -3,10 +3,13 @@
 NAME="AHMAD"
 TOTAL=25
 
+# list all with file NAME and REMOVE NAME for only get number and get latest number
 CHECK_NUMBER=$(ls -v ${NAME}* | sed "s/${NAME}//" | tail -1)
 
+# check if CHECK_NUMBER start from 0 and then + 1 or start 25 and then + 1
 MIN_NUMBER=$(( ${CHECK_NUMBER:-0} + 1 ))
 
+# subs for max iteration, if MIN_NUMBER start 1 and then + 25 - 1 equals max iteration is 25
 MAX_NUMBER=$(( ${MIN_NUMBER} + ${TOTAL} - 1 ))
 
 for ((i=${MIN_NUMBER}; i<=${MAX_NUMBER}; i++))
